94874f6463e8f53a1ddc3e99403fb2d2552f4cd2
dojo2023-saito-keita <130334721+dojo2023-saito-keita@users.noreply.github.com>
2023-06-12 17:33:12 +0900

    ファイルの作成

--- src/src/dao/UserDAO.java
+++ src/src/dao/UserDAO.java
@@ -8,50 +8,89 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.User;
+
 public class UserDAO {
 	// 引数paramで検索項目を指定し、検索結果のリストを返す
-	public List<Bc> select(Bc param) {
+	public boolean selectId(User param) {
 		Connection conn = null;
-		List<Bc> cardList = new ArrayList<Bc>();
-
+		boolean loginResult = false;
 		try {
 			// JDBCドライバを読み込む
 			Class.forName("org.h2.Driver");
-
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
-
-			// SQL文を準備する
-			String sql = "select NUMBER, NAME, ADDRESS from BC WHERE NUMBER LIKE ? AND NAME LIKE ? AND ADDRESS LIKE ? ORDER BY NUMBER";
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/NMW", "sa", "");
+			// SELECT文を準備する
+			String sql = "select count(*) from users where u_id = ? and password = ? position = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, param.getU_id());
+			pStmt.setString(2, param.getPassword());
+			pStmt.setString(3, param.getPosition());
+			// SELECT文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+			// ユーザーIDとパスワードが一致するユーザーがいたかどうかをチェックする
+			rs.next();
+			if (rs.getInt("count(*)") == 1) {
+				loginResult = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			loginResult = false;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			loginResult = false;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					loginResult = false;
+				}
+			}
+		}
+		// 結果を返す
+		return loginResult;
+	}
 
-			// SQL文を完成させる
-			if (param.getNumber() != null) {
-				pStmt.setString(1, "%" + param.getNumber() + "%");
+	public List<User> select(User param) {
+		Connection conn = null;
+		List<User> cardList = new ArrayList<User>();
+		try {
+			// JDBCドライバを読み込む  Javaによるデータベース接続
+			Class.forName("org.h2.Driver");
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/NMW", "sa", "");
+			// SQL文を準備する  （検索するSQL文）検索する項目増やせる
+			String sql = "select users.u_id, user_likefoods.lf_id,  from users left join user_likefoods on users.u_id = user_likefoods.u_id "
+					+ "left join user_dislikefoods on users.u_id = user_dislikefoods.u_id "
+					+ "left join foods on foods.f_id = user_likefoods.lf_id or foods.f_id = user_dislikefoods.df_id"
+					+ "where users.u_id = ? ";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			// SQL文を完成させる(?を埋める)
+			if (param.getU_id() != null) {
+				pStmt.setString(1, param.getU_id()); // %はSQLのあいまい検索のやつ ?を"%" + param.getNumber() + "%"にしてる
 			} else {
 				pStmt.setString(1, "%");
 			}
-			if (param.getName() != null) {
-				pStmt.setString(2, "%" + param.getName() + "%");
-			} else {
-				pStmt.setString(2, "%");
-			}
-			if (param.getAddress() != null) {
-				pStmt.setString(3, "%" + param.getAddress() + "%");
-			} else {
-				pStmt.setString(3, "%");
-			}
-
+			// ここから何も触ってないです
 			// SQL文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();
-
-			// 結果表をコレクションにコピーする
+			// 結果表をコレクションにコピーする （通常のArrayリストに入れている）
 			while (rs.next()) {
 				Bc card = new Bc(
 						rs.getString("NUMBER"),
+						rs.getString("COMPANY_NAME"),
+						rs.getString("DEPT_NAME"),
+						rs.getString("POSITION_NAME"),
 						rs.getString("NAME"),
-						rs.getString("ADDRESS"));
-				;
+						rs.getString("ZIPCODE"),
+						rs.getString("ADDRESS"),
+						rs.getString("TEL_NUMBER"),
+						rs.getString("FAX"),
+						rs.getString("EMAIL"),
+						rs.getString("NOTE"));
 				cardList.add(card);
 			}
 		} catch (SQLException e) {
@@ -71,44 +110,47 @@ public class UserDAO {
 				}
 			}
 		}
-
 		// 結果を返す
 		return cardList;
 	}
 
-	// 引数cardで指定されたレコードを登録し、成功したらtrueを返す
-	public boolean insert(Bc card) {
+	public boolean insert(User card) {
 		Connection conn = null;
 		boolean result = false;
-
 		try {
 			// JDBCドライバを読み込む
 			Class.forName("org.h2.Driver");
-
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
-
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/NMW", "sa", "");
 			// SQL文を準備する
-			String sql = "insert into BC (NUMBER, NAME, ADDRESS) values (?, ?, ?)";
+			String sql = "insert into User (u_id, password,position,lf_id ,df_id) values (?, ?, ?, ?, ?)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-
 			// SQL文を完成させる
-			if (card.getNumber() != null && !card.getNumber().equals("")) {
-				pStmt.setString(1, card.getNumber());
+			if (card.getU_id() != null && !card.getU_id().equals("")) {
+				pStmt.setString(1, card.getU_id);
 			} else {
 				pStmt.setString(1, null);
 			}
-			if (card.getName() != null && !card.getName().equals("")) {
-				pStmt.setString(2, card.getName());
+			if (card.getPassword() != null && !card.getPassword().equals("")) {
+				pStmt.setString(2, card.getPassword());
 			} else {
 				pStmt.setString(2, null);
 			}
-			if (card.getAddress() != null && !card.getAddress().equals("")) {
-				pStmt.setString(3, card.getAddress());
+			if (card.getPosition() != null && !card.getPosition().equals("")) {
+				pStmt.setString(3, card.getPosition());
 			} else {
 				pStmt.setString(3, null);
 			}
-
+			if (card.getLf_id() != null && !card.getLf_id().equals("")) {
+				pStmt.setString(4, card.getLf_id());
+			} else {
+				pStmt.setString(4, null);
+			}
+			if (card.getDf_id() != null && !card.getDf_id().equals("")) {
+				pStmt.setString(5, card.getDf_id());
+			} else {
+				pStmt.setString(5, null);
+			}
 			// SQL文を実行する
 			if (pStmt.executeUpdate() == 1) {
 				result = true;
@@ -127,40 +169,39 @@ public class UserDAO {
 				}
 			}
 		}
-
 		// 結果を返す
 		return result;
 	}
 
 	// 引数cardで指定されたレコードを更新し、成功したらtrueを返す
-	public boolean update(Bc card) {
+	public boolean update(User user) {
 		Connection conn = null;
 		boolean result = false;
-
 		try {
 			// JDBCドライバを読み込む
 			Class.forName("org.h2.Driver");
-
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
-
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/NMW", "sa", "");
 			// SQL文を準備する
-			String sql = "update BC set NAME=?, ADDRESS=? where NUMBER=?";
+			String sql = "update users set password=?, lf_id=?, df_id=? where u_id=?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-
 			// SQL文を完成させる
-			if (card.getName() != null && !card.getName().equals("")) {
-				pStmt.setString(1, card.getName());
+			if (user.getPassword() != null && !user.getPassword().equals("")) {
+				pStmt.setString(1, user.getPassword());
 			} else {
 				pStmt.setString(1, null);
 			}
-			if (card.getAddress() != null && !card.getAddress().equals("")) {
-				pStmt.setString(2, card.getAddress());
+			if (user.getLf_id() != null && !user.getLf_id().equals("")) {
+				pStmt.setString(2, user.getLf_id());
 			} else {
 				pStmt.setString(2, null);
 			}
-			pStmt.setString(3, card.getNumber());
-
+			if (user.getDf_id() != null && !user.getDf_id().equals("")) {
+				pStmt.setString(3, user.getDf_id());
+			} else {
+				pStmt.setString(3, null);
+			}
+			pStmt.setString(4, user.getU_id());
 			// SQL文を実行する
 			if (pStmt.executeUpdate() == 1) {
 				result = true;
@@ -179,7 +220,6 @@ public class UserDAO {
 				}
 			}
 		}
-
 		// 結果を返す
 		return result;
 	}
@@ -188,21 +228,16 @@ public class UserDAO {
 	public boolean delete(String number) {
 		Connection conn = null;
 		boolean result = false;
-
 		try {
 			// JDBCドライバを読み込む
 			Class.forName("org.h2.Driver");
-
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
-
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/NMV", "sa", "");
 			// SQL文を準備する
-			String sql = "delete from BC where NUMBER=?";
+			String sql = "delete from users where u_id=?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-
 			// SQL文を完成させる
 			pStmt.setString(1, number);
-
 			// SQL文を実行する
 			if (pStmt.executeUpdate() == 1) {
 				result = true;
@@ -221,7 +256,6 @@ public class UserDAO {
 				}
 			}
 		}
-
 		// 結果を返す
 		return result;
 	}
