0595244b128bdf89c4f52497f9c9968a07d7dd27
dojo2023-uchida <KentoUchidaplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-13 13:36:47 +0900

    冷蔵庫

--- src/src/model/Refrigerators.java
+++ src/src/model/Refrigerators.java
@@ -1,14 +1,28 @@
-package model;
+ package model;
+
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+//「implements Serializable」の追加
 public class Refrigerators implements Serializable{
-	private int ref_id;
-	private String u_id;
-	private int f_id;
-	private double f_count;
 
-	public Refrigerators
+	// Refrigeratorを元に追加
+	private List<Refrigerator>refrigerators = new ArrayList<Refrigerator>();
+
+	// コンストラクタの追加
+	public Refrigerators(Refrigerator refrigerator) {
+		this.refrigerators.add(refrigerator);
+	}
+
+	public List<Refrigerator> getRefrigerators() {
+		return refrigerators;
+	}
+
+	public void setRefrigerators(List<Refrigerator> refrigerator) {
+		this.refrigerators = refrigerator;
+	}
+
 }
+
