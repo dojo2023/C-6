ac9b32e6c635d3033c4cf2931ea907f68be4e972
dojo2023-saito-keita <130334721+dojo2023-saito-keita@users.noreply.github.com>
2023-06-13 13:25:51 +0900

    更新

--- src/src/model/AdmUser.java
+++ src/src/model/AdmUser.java
@@ -1,5 +1,41 @@
 package model;
 
-public class AdmUser {
+import java.io.Serializable;
 
+public class AdmUser implements Serializable{
+	// Userを元に追加
+	private String u_id;
+	private String password;
+	private int position = 2; // 管理者：2
+
+	// コンストラクタの追加
+	public AdmUser(String u_id, String password) {
+		this.u_id = u_id;
+		this.password = password;
+	}
+
+	// get, setメソッドの追加
+	public String getU_id() {
+		return u_id;
+	}
+
+	public void setU_id(String u_id) {
+		this.u_id = u_id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
 }
--- src/src/model/Refrigerators.java
+++ src/src/model/Refrigerators.java
@@ -1,5 +1,14 @@
 package model;
 
-public class Refrigerators {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Refrigerators implements Serializable{
+	private int ref_id;
+	private String u_id;
+	private int f_id;
+	private double f_count;
+
+	public Refrigerators
 }
