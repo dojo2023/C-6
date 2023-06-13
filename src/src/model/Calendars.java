b6b2eb0509c02cd57c4107f0a1528703334ddc46
dojo2023-kosaka <130334538+dojo2023-kosaka@users.noreply.github.com>
2023-06-13 13:44:17 +0900

    カレンダー

--- src/src/model/Calendars.java
+++ src/src/model/Calendars.java
@@ -1,5 +1,23 @@
 package model;
 
-public class Calendars {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Calendars implements Serializable{
+	private List<Calendar>Calendars = new ArrayList<Calendar>();
+
+		// コンストラクタの追加
+		public Calendars(Calendar Calendar) {
+			this.Calendars.add(Calendar);
+		}
+
+		// get, setメソッドの追加
+		public List<Calendar> getCalendars() {
+			return Calendars;
+		}
+
+		public void setCalendars(List<Calendar> calendar) {
+			Calendars = calendar;
+		}
 }
