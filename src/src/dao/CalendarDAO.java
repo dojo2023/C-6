2c450f89dfabaa38db984d64c85d2937a9710ee1
dojo2023-nagashima <130334695+dojo2023-nagashima@users.noreply.github.com>
2023-06-14 15:00:10 +0900

    カレンダーDao

--- src/src/dao/CalendarDAO.java
+++ src/src/dao/CalendarDAO.java
@@ -1,5 +1,204 @@
 package dao;
 
-public class CalendarDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Calendar;
+
+public class CalendarDAO {
+	// 引数userで検索項目を指定し、検索結果のリストを返す
+	public List<Calendar> select(Calendar calendar) {
+		Connection conn = null;
+		List<Calendar> CalendarList = new ArrayList<Calendar>();
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/NMW", "sa", "");
+			// SELECT文を準備する
+			String sql = "select calendars.u_id ,calendars.rec_id , calendars.date ,calendars.c_count ,"
+					+ " recipes.r_name ,recipes.cooking_expenses ,recipes.eating_out_expenses"
+					+ "from calendars left join recipes"
+					+ "on calendars.rec_id = recipes.rec_id"
+					+ "where calendars.u_id = ? ";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる(?を埋める)
+			if (calendar.getU_id() != null) {
+				pStmt.setString(1, calendar.getU_id()); // %はSQLのあいまい検索のやつ ?を"%" + param.getNumber() + "%"にしてる
+			} else {
+				pStmt.setString(1, "%");
+
+			}
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			while (rs.next()) {
+				Calendar c = new Calendar(
+						rs.getString("calendars.u_id"),
+						rs.getInt("calendars.rec_id"),
+						rs.getDate("calendars.date"),
+						rs.getInt("calendars.c_count"),
+						rs.getString("recipes.r_name"),
+						rs.getInt("recipes.cooking_expenses"),
+						rs.getInt("recipes.eating_out_expenses"));
+
+				CalendarList.add(c);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			CalendarList = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			CalendarList = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					CalendarList = null;
+				}
+			}
+		}
+		// 結果を返す
+		return CalendarList;
+	}
+
+	public boolean insert(Calendar calendar) {
+		Connection conn = null;
+		boolean calendarResult = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/NMW", "sa", "");
+
+			// SQL文を準備する
+			String sql = "insert into calendars (u_id, rec_id, date, c_count)"
+					   + " values (?, ?, ?, ?)";
+
+
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+
+			// SQL文を完成させる
+			if (calendar.getU_id() != null && !calendar.getU_id().equals("")) {
+				pStmt.setString(1, calendar.getU_id());
+			} else {
+				pStmt.setString(1, null);
+			}
+			if (calendar.getRec_id() != -1 ) {
+				pStmt.setInt(2, calendar.getRec_id());
+			} else {
+				pStmt.setInt(2, -1);
+			}
+			if (calendar.getDate() != null) {
+				pStmt.setDate(3, calendar.getDate());
+			} else {
+				pStmt.setString(3, null);
+			}
+			if (calendar.getC_count() != -1) {
+				pStmt.setInt(4, calendar.getC_count());
+			} else {
+				pStmt.setInt(4, -1);
+			}
+
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				calendarResult = true;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return calendarResult;
+	}
+
+	// 引数cardで指定されたレコードを更新し、成功したらtrueを返す
+	public boolean update(Calendar calendar) {
+		Connection conn = null;
+		boolean calendarResult = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/NMW", "sa", "");
+
+			// SQL文を準備する
+			String sql = "update calendars set c_count=? where u_id=? and rec_id=? and date=?";
+
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+
+			// SQL文を完成させる
+			if (calendar.getC_count() != -1) {
+				pStmt.setInt(1, calendar.getC_count());
+			} else {
+				pStmt.setInt(1, -1);
+			}
+			if (calendar.getU_id() != null && !calendar.getU_id().equals("")) {
+				pStmt.setString(2, calendar.getU_id());
+			} else {
+				pStmt.setString(2, null);
+			}
+			if (calendar.getRec_id() != -1) {
+				pStmt.setInt(3, calendar.getRec_id());
+			} else {
+				pStmt.setInt(3, -1);
+			}
+			if (calendar.getDate() != null) {
+				pStmt.setDate(4, calendar.getDate());
+			} else {
+				pStmt.setDate(4, null);
+			}
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1 ) {
+				calendarResult = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return calendarResult;
+	}
 }
--- src/src/model/Calendar.java
+++ src/src/model/Calendar.java
@@ -8,9 +8,12 @@ public class Calendar implements Serializable {
 	private int rec_id;
 	private Date date;
 	private int c_count;
+	private String r_name;
+	private int cooking_expenses;
+	private int eating_out_expenses;
 
 	// 2種類のコンストラクタの追加
-	public Calendar(String u_id, int rec_id, int c_count) {
+	public Calendar(String u_id, int rec_id, int c_count, String r_name, int cooking_expenses, int eating_out_expenses) {
 		Date sqlNow = new Date(System.currentTimeMillis()); // util.date => sql.dateへの変換
 		java.util.Date utilDate = sqlNow;
 
@@ -18,6 +21,19 @@ public class Calendar implements Serializable {
 		this.rec_id = rec_id;
 		this.date = new Date(utilDate.getTime());
 		this.c_count = c_count;
+		this.r_name = r_name;
+		this.cooking_expenses = cooking_expenses;
+		this.eating_out_expenses = eating_out_expenses;
+	}
+
+	public Calendar(String u_id, int rec_id, Date date, int c_count, String r_name, int cooking_expenses, int eating_out_expenses) {
+		this.u_id = u_id;
+		this.rec_id = rec_id;
+		this.date = date;
+		this.c_count = c_count;
+		this.r_name = r_name;
+		this.cooking_expenses = cooking_expenses;
+		this.eating_out_expenses = eating_out_expenses;
 	}
 
 	public Calendar() {
@@ -28,6 +44,16 @@ public class Calendar implements Serializable {
 		this.rec_id = -1;
 		this.date = new Date(utilDate.getTime());
 		this.c_count = -1;
+		this.r_name = "";
+		this.cooking_expenses = -1;
+		this.eating_out_expenses = -1;
+	}
+
+	public Calendar(String u_id, int rec_id, Date date, int c_count) {
+		this.u_id = u_id;
+		this.rec_id = rec_id;
+		this.date = date;
+		this.c_count = c_count;
 	}
 
 	// get, setメソッドの追加
@@ -61,5 +87,29 @@ public class Calendar implements Serializable {
 
 	public void setC_count(int c_count) {
 		this.c_count = c_count;
+	}
+
+	public String getR_name() {
+		return r_name;
+	}
+
+	public void setR_name(String r_name) {
+		this.r_name = r_name;
+	}
+
+	public int getCooking_expenses() {
+		return cooking_expenses;
+	}
+
+	public void setCooking_expenses(int cooking_expenses) {
+		this.cooking_expenses = cooking_expenses;
+	}
+
+	public int getEating_out_expenses() {
+		return eating_out_expenses;
+	}
+
+	public void setEating_out_expenses(int eating_out_expenses) {
+		this.eating_out_expenses = eating_out_expenses;
 	}
 }
