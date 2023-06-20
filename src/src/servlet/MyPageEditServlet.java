9a6f6f24ca3aa00093d3173a913023cab91708fa
dojo2023-nagashima <130334695+dojo2023-nagashima@users.noreply.github.com>
2023-06-20 17:49:51 +0900

    マイページexit

--- src/src/servlet/MyPageEditServlet.java
+++ src/src/servlet/MyPageEditServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -36,7 +37,7 @@ public class MyPageEditServlet extends HttpServlet {
 		UserDAO uDAO = new UserDAO();
 
 		//MyPageからユーザー情報の受け取り
-		LoginUser loginUser = (LoginUser)request.getAttribute("loginUser");
+		LoginUser loginUser = (LoginUser)session.getAttribute("loginUser");
 
 
 		List<User> user_inf = uDAO.selectLfDf(new User(loginUser.getId()));
@@ -59,8 +60,64 @@ public class MyPageEditServlet extends HttpServlet {
 			response.sendRedirect("/NMW/LoginServlet");
 			return;
 		}
+		//編集ボタン
+		if ((request.getParameter("edit")) == "1") {
+
+			LoginUser loginUser = (LoginUser)session.getAttribute("id");
+			UserDAO uDAO = new UserDAO();
+			uDAO.delete(loginUser.getId());
+
+			String u_id = request.getParameter("ID");
+			String password = request.getParameter("PW");
+
+			List<Integer> lf_id = new ArrayList<Integer>();
+			String lf_id_s = (String)request.getAttribute("Lf_id");
+			if (lf_id_s != null) {	// null判定
+				String lf_id_s_l[] = lf_id_s.split(",");
+				for(int i = 0; i < lf_id_s_l.length;i++) {
+					lf_id.add(Integer.parseInt(lf_id_s_l[i]));
+				}
+			} else {
+				lf_id.add(-1);	// nullの場合は-1
+			}
 
 
+			List<Integer> df_id = new ArrayList<Integer>();
+			String df_id_s = (String)request.getAttribute("Df_id");
+			if (df_id_s != null) {
+				String df_id_s_l[] = df_id_s.split(",");
+				for(int i = 0; i < df_id_s_l.length;i++) {
+					df_id.add(Integer.parseInt(df_id_s_l[i]));
+				}
+			} else {
+				df_id.add(-1);
+			}
+
+
+			// 登録処理を行う
+			// 登録成功
+			UserDAO rDAO = new UserDAO();
+			rDAO.insert(new User(u_id, password, lf_id, df_id));
+
+				//マイページへフォワード
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/myPage.jsp");
+				dispatcher.forward(request, response);
+
+		}else {
+
+			LoginUser loginUser = (LoginUser)session.getAttribute("id");
+			UserDAO uDAO = new UserDAO();
+			uDAO.delete(loginUser.getId());
+
+			//ユーザーIDの削除
+			session.invalidate();
+
+			//ログインページへフォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+			dispatcher.forward(request, response);
+		}
+
+		}
 	}
 
-}
+
