dade0f4b114f888a177e123de5678e41091465db
dojo2023-saito-keita <130334721+dojo2023-saito-keita@users.noreply.github.com>
2023-06-20 14:53:38 +0900

    update

--- src/WebContent/js/login.js
+++ src/WebContent/js/login.js
@@ -1,3 +1,12 @@
-/**
- * 
- */
+function check(){
+	/* [ログイン]ボタンをクリックしたときの処理 */
+	var formObj = document.getElementById('login_form');
+	var errorMessageObj = document.getElementById('error_message');
+
+	if (!formObj.ID.value || !formObj.PW.value) {
+		errorMessageObj.textContent = '※IDとパスワードを入力してください！';
+		return false;
+	} else {
+		return true;
+	}
+}
