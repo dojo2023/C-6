d77d915a73285073a767076b38f986f00c1aab79
dojo2023-saito-keita <130334721+dojo2023-saito-keita@users.noreply.github.com>
2023-06-23 14:59:18 +0900

    Update UserDAO.java

--- src/src/dao/UserDAO.java
+++ src/src/dao/UserDAO.java
@@ -68,7 +68,7 @@ public class UserDAO {
 			String sql_lf = "select users.u_id, users.password, user_likefoods.lf_id from users "
 					+ "left join user_likefoods on users.u_id = user_likefoods.u_id "
 					+ "where users.u_id = ?";
-			String sql_df = "select users.u_id, user_dislikefoods.df_id from users "
+			String sql_df = "select users.u_id, users.password, user_dislikefoods.df_id from users "
 					+ "left join user_dislikefoods on users.u_id = user_dislikefoods.u_id "
 					+ "where users.u_id = ?";
 
@@ -91,26 +91,17 @@ public class UserDAO {
 			ResultSet rs_df = pStmt_df.executeQuery();
 
 			// 結果表をコレクションにコピーする （通常のArrayリストに入れている）
-			// 1行目をスキップするかも
-			boolean f1 = rs_lf.next();
-			boolean f2 = rs_df.next();
-
 			List<Integer> rs_lf_l = new ArrayList<Integer>();
 			List<Integer> rs_df_l = new ArrayList<Integer>();
 
-			while (f1 || f2) {
-				User card = null;
-				if(f1 && f2) {
-					rs_lf_l.add(rs_lf.getInt("user_likefoods.lf_id"));
-					rs_df_l.add(rs_df.getInt("user_dislikefoods.df_id"));
-				}else if (f1) {
-					rs_lf_l.add(rs_lf.getInt("user_likefoods.lf_id"));
-				}else if (f2) {
-					rs_df_l.add(rs_df.getInt("user_dislikefoods.df_id"));
-				}
+			while (rs_lf.next()) {
+				rs_lf_l.add(rs_lf.getInt("user_likefoods.lf_id"));
+				System.out.println(rs_lf.getString("user_likefoods.lf_id"));
+			}
 
-				f1 = rs_lf.next();
-				f2 = rs_df.next();
+			while (rs_df.next()) {
+				rs_df_l.add(rs_df.getInt("user_dislikefoods.df_id"));
+				System.out.println(rs_df.getString("user_dislikefoods.df_id"));
 			}
 
 			User card = null;
